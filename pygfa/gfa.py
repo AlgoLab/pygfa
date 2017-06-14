2dc6af4e94040a95e5a207a9b5618f4f0f0a7a5c
dlobba <d.lobba@campus.unimib.it>
2017-06-14 15:22:40 +0200

    Changed behaviour for nodes and edges access

--- pygfa/gfa.py
+++ pygfa/gfa.py
@@ -1,9 +1,10 @@
-import sys
-import re
 from parser.lines import header, segment, link, containment, path
 from parser.lines import edge, gap, fragment, group
 from graph_element import node, edge as ge
+import copy
 import networkx as nx
+
+class InvalidSearchParameters (Exception): pass
 
 class GFA (nx.MultiGraph):
     """GFA will use a networkx MultiGraph as structure to contain the elements
@@ -12,24 +13,78 @@ class GFA (nx.MultiGraph):
 
     def __init__ (self):
         # a virtual id assigned to edges (graph edges) that don't have an id.
-        # Their id will be virtual_# where # will be given by next_virtual_id.
+        # Their id will be 'virtual_#' where # will be given by next_virtual_id.
         self.next_virtual_id = 0;
         self._graph = nx.MultiGraph ()
 
     # Simulate networkx graph behaviour with composition.
     def nodes (self, **kwargs):
-        return self._graph.nodes (**kwargs)
+        """Return a copy list of all the nodes in the graph."""
+        return copy.deepcopy (self._graph.nodes (**kwargs))
 
     def edges (self, **kwargs):
-        return self._graph.edges (**kwargs)
+        """Return a copy list of all the edges in the graph."""
+        return copy.deepcopy (self._graph.edges (**kwargs))
 
-    @property
-    def node (self):
-        return self._graph.node
+#    @property
+#    def node (self):
+#        return self._graph.node
 
-    @property
-    def edge (self):
-        return self._graph.edge
+#    @property
+#    def edge (self):
+#        return self._graph.edge
+
+    # TODO:
+    # If this modifications are accepted we could
+    # change the name of get_node and get_edge to node and
+    # edge like above
+    def get_node (self, node_id):
+        if node_id in self._graph.node:
+            return self._graph.node[node_id]
+        return None
+
+    def get_edges (self, **kwargs):
+        return selg._graph.edges (**kwargs)
+    
+    def get_edge (self, identifier='*'):
+        """An interface to the edge method present in networkx.
+        If '*' is given returns all the individual edges in the graph.
+        """
+        if identifier == '*':
+            return self._graph.edge
+        if isinstance (identifier, tuple) and len (identifier) >= 2:
+            return self.search_edge_by_nodes (identifier)
+        else:
+            return self.search_edge_by_key (identifier)
+
+        
+    def search_edge_by_key (self, key):
+        for from_node, to_node, edge_key in self._graph.edges_iter (keys=True):
+            if key == edge_key:
+                return self._graph.edge[from_node][to_node][edge_key]
+        return None
+
+    def search_edge_by_nodes (self, nodes):
+        """If given a tuple with from_node and to_node return all the edges
+        between the two nodes.
+        If a third element is present in the tuple it return the exact edge between
+        the two nodes with the key specified by the third element.
+        If no match is found return None."""
+        if len (nodes) < 2:
+            raise InvalidSearchParameters ()
+        from_node = nodes [0]
+        to_node = nodes [1]
+
+        try:
+            if len (nodes) > 2:
+                key = nodes[2]
+                return self._graph.edge[from_node][to_node][key]
+            
+            return self._graph.edge[from_node][to_node]
+        except:
+            return None
+
+
 
     def clear (self):
         """Call networkx 'clear' method and reset the virtual id counter."""
@@ -46,7 +101,7 @@ class GFA (nx.MultiGraph):
 
         self._graph.add_node (\
                                   new_node.nid, id=new_node.nid, sequence=new_node.sequence, \
-                                  slen=new_node.slen, opt_fields=new_node.opt_fields)
+                                  slen=new_node.slen, **new_node.opt_fields)
         return True
         
 
@@ -56,7 +111,6 @@ class GFA (nx.MultiGraph):
         edge id will be preserved as edge attribute. All edge attributes will be stored as
         netwrorkx edge attributes and its optfields will be store as a separate list in the edge
         within the attribute 'opt_fields'."""
-        print (ge.is_edge (new_edge))
         if not ge.is_edge (new_edge):
             raise ge.InvalidEdgeError ("The object given is not an Edge.")
 
@@ -76,7 +130,7 @@ class GFA (nx.MultiGraph):
             self.next_virtual_id += 1
         
         self._graph.add_edge ( \
-                                   from_node, to_node, key, \
+                                   from_node, to_node, key=key, \
                                    id=new_edge.eid, \
                                    from_node = new_edge.from_node, \
                                    to_node = new_edge.to_node, \
@@ -85,6 +139,6 @@ class GFA (nx.MultiGraph):
                                    alignment = new_edge.alignment, \
                                    displacement = new_edge.displacement ,\
                                    variance = new_edge.variance, \
-                                   opt_fields = new_edge.opt_fields \
+                                   **new_edge.opt_fields \
                                    )
         return True
--- test/test_gfa_graph.py
+++ test/test_gfa_graph.py
@@ -21,10 +21,10 @@ class TestLine (unittest.TestCase):
 
         print ("\nNODES:\n{0}".format (self.graph.nodes(data=True)))
         self.assertTrue (len (self.graph.nodes ()) == 1)
-        self.assertTrue (self.graph.node['3']['sequence'] == "TGCAACGTATAGACTTGTCAC")
-        self.assertTrue (self.graph.node['3']['id'] == "3")
-        self.assertTrue (self.graph.node['3']['opt_fields']['ui'].value == "test")
-        self.assertTrue (self.graph.node['3']['opt_fields']['ui'].type == "Z")
+        self.assertTrue (self.graph.get_node('3')['sequence'] == "TGCAACGTATAGACTTGTCAC")
+        self.assertTrue (self.graph.get_node('3')['id'] == "3")
+        self.assertTrue (self.graph.get_node('3')['ui'].value == "test")
+        self.assertTrue (self.graph.get_node('3')['ui'].type == "Z")
 
 
     def test_add_edge (self):
@@ -46,22 +46,30 @@ class TestLine (unittest.TestCase):
         edg = ge.Edge.from_line (line)
         self.graph.add_edge (edg)
 
-        print (self.graph.edge)
+        print (self.graph.get_edge('*'))
 
         
         print ("NODES:\n{0}".format (self.graph.nodes (data=True)))
-        print ("\nEDGES:\n{0}".format (self.graph.edges(data=True)))
+        print ("\nEDGES:\n{0}".format (self.graph.get_edge(('3','4'))))
 
-        self.assertTrue (len (self.graph.edge['3']['4']) == 2)
+        self.assertTrue (len (self.graph.get_edge(('3', '4'))) == 2)
 
         # a virtual id is given to F and L
-        print ("\nEDGES BETWEEN 3 AND 4\n{0}".format (self.graph.edge['3']['4']))
+        print ("\nEDGES BETWEEN 3 AND 4\n{0}".format (self.graph.get_edge(('3','4'))))
         self.assertTrue (len (self.graph.nodes ()) == 2)
 
         # The F line is added first so it will have id 'virtual_0'
-        self.assertTrue (self.graph.edge['3']['4']['virtual_0']['from_node'] == "3")
-        
-        
+        # This first test get all the edges between node 3 and 4 and the get the edge labelled 'virtual_0'
+        self.assertTrue (self.graph.get_edge(('3','4'))['virtual_0']['from_node'] == "3")
+
+        # This test instead get instantly the edge labelled 'virtual_0', that is unique
+        # in the graph
+        self.assertTrue (self.graph.get_edge('virtual_0')['from_node'] == "3")
+
+        # The default behaviour of get_edge is to call edge on the graph, so
+        # it returns all the edges in the graph
+        self.assertTrue (len (self.graph.get_edge ()) == 2)
+        print ("\nget_edge ('*'):\n{0}\n".format (self.graph.get_edge ()))
 
 
 if  __name__ == '__main__':
