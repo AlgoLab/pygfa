4eac35f0f1e46a5c2065e670446924a015a6d0bd
dlobba <d.lobba@campus.unimib.it>
2017-07-22 11:48:05 +0200

    Added connected components as subgraphs

--- pygfa/dovetail_operations/components/connected.py
+++ pygfa/dovetail_operations/components/connected.py
@@ -1,3 +1,11 @@
+"""
+Algorithms to find connected components in the graph considering
+only dovetails overlaps.
+
+Adapted using the networkx connected module:
+networkx/networkx/algorithms/components/connected.py 
+"""
+
 def _plain_bfs_dovetails(gfa_, source):
     if source not in gfa_:
         return ()
@@ -16,12 +24,30 @@ def _plain_bfs_dovetails(gfa_, source):
 def dovetails_nodes_connected_component(gfa_, source):
     return set(_plain_bfs_dovetails(gfa_, source))
 
-def dovetails_nodes_connected_components(gfa_):
+def dovetails_nodes_connected_components(gfa_, with_sequence=True):
+    """Compute the connected components in the GFA graph.
+
+    :param with_sequence: If set start the computation considering
+        only nodes where the 'sequence' propery is present. Consider
+        every nodes in the graph otherwise.
+    """
     seen = set()
-    dovetail_nodes = gfa_.dovetails_nbunch_iter()
-    for v in dovetail_nodes:
+    nodes = gfa_.nodes(with_sequence=with_sequence)
+    for v in nodes:
         if v not in seen:
             c = set(_plain_bfs_dovetails(gfa_, v))
             yield c
             seen.update(c)
+
+def dovetails_connected_components_subgraphs(gfa_, copy=True, with_sequence=True):
+    """Generate connected components as subgraphs.
+
+    :return comp: A generator of graphs, one for each connected
+    component of gfa_.
+    """
+    for c in dovetails_nodes_connected_components(gfa_, with_sequence=with_sequence):
+        if copy:
+            yield gfa_.dovetails_subgraph(c).copy()
+        else:
+            yield gfa_.dovetails_subgraph(c)
 
