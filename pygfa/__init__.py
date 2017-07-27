ed7ff0f9ff4522102fe2c50be0bffe996c2ba5d6
dlobba <d.lobba@campus.unimib.it>
2017-07-27 10:09:07 +0200

    Imported required script when pygfa is imported

--- pygfa/__init__.py
+++ pygfa/__init__.py
@@ -1,3 +1,5 @@
 from pygfa import gfa
 from pygfa.dovetail_operations.components.connected import *
-
+from pygfa.dovetail_operations.components.biconnected import *
+from pygfa.dovetail_operations.components.biconnected import _biconnected_dfs
+from pygfa.dovetail_operations.linear_paths import *
