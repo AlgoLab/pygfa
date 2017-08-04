779f3ac92bd016c677f3329ef63d1fe14b0fe55d
dlobba <d.lobba@campus.unimib.it>
2017-08-04 18:29:58 +0200

    Simplied import chain

--- pygfa/__init__.py
+++ pygfa/__init__.py
@@ -1,5 +1,2 @@
 from pygfa import gfa
-from pygfa.dovetail_operations.components.connected import *
-from pygfa.dovetail_operations.components.biconnected import *
-from pygfa.dovetail_operations.components.biconnected import _biconnected_dfs
-from pygfa.dovetail_operations.linear_paths import *
+from pygfa.operations import * # common operations
