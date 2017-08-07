06aa22b141b3027586bf3523830f86ad091c0907
dlobba <d.lobba@campus.unimib.it>
2017-08-07 17:23:23 +0200

    Added test case to all simple path and fixed some issue thanks to TDD

--- pygfa/algorithms/simple_paths.py
+++ pygfa/algorithms/simple_paths.py
@@ -11,13 +11,17 @@ The same documentation for networkx is valid using this algorithms."""
 #    BSD license.
 import networkx as nx
 
-def all_simple_paths(gfa_, source, target, selector, edges=False, cutoff=None, **args):
+__all__ = ["all_simple_paths"]
+
+def all_simple_paths(gfa_, source, target, selector, edges=False, keys=True, cutoff=None):
     """Compute the all_simple_path algorithm as described in 
     networkx, but return the edges keys if asked and use the
     given selector to obtain the nodes to consider.
 
     :param selector: A function or a method used to select the nodes
-        to consider.
+        to consider, the selector MUST give back two values at least and
+        three values considering the keys. So the selector must be a
+        similar networkx edges selectors (at least in behavior).
     :param edges: If True return the edges key that connect each pair of nodes
         in the simple path, each data is given in the format
         `(node_to, edge_that_connect_previous_to_node_to)`, so
@@ -31,17 +35,17 @@ def all_simple_paths(gfa_, source, target, selector, edges=False, cutoff=None, *
     if cutoff is None:
         cutoff = len(gfa_.nodes())-1
     if edges is False:
-        return _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=cutoff, **args)
+        return _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=cutoff)
     else:
-        return _all_simple_paths_edges_multigraph(gfa_, source, target, selector, cutoff=cutoff, **args)
+        return _all_simple_paths_edges_multigraph(gfa_, source, target, selector, keys=keys, cutoff=cutoff)
 
 
-def _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=None, **args):
+def _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=None):
     if cutoff < 1:
         return
     visited = [source]
     # stack = [(v for u,v in gfa_.edges(source))]
-    stack = [(v for u,v in selector(source, **args))]
+    stack = [(v for u,v in selector(source))]
     while stack:
         children = stack[-1]
         child = next(children, None)
@@ -54,7 +58,7 @@ def _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=None, **
             elif child not in visited:
                 visited.append(child)
                 # stack.append((v for u,v in gfa_.edges(child)))
-                stack.append((v for u,v in selector(child, **args)))
+                stack.append((v for u,v in selector(child)))
         else: #len(visited) == cutoff:
             count = ([child]+list(children)).count(target)
             for i in range(count):
@@ -62,43 +66,59 @@ def _all_simple_paths_multigraph(gfa_, source, target, selector, cutoff=None, **
             stack.pop()
             visited.pop()
 
-def _all_simple_paths_edges_multigraph(gfa_, source, target, selector, cutoff=None, **args):
+def _all_simple_paths_edges_multigraph(gfa_, source, target, selector, keys=False, cutoff=None):
     """Return all simple paths from source to target with
     all the edges id that connect each pair of nodes.
     """
     # the algorithm has been extended to work with
-    # a list of tuples, in the form (node, edge_from_previous_to_this_node)
+    # a list of tuples, in the form (from_, to_, edge_from_previous_to_this_node)
     # so in order to avoid the extraction of the same node
     # with different edges (generating duplication of paths)
     # the nodes are always extracted in the main
     # comparations (i.e. child not in visited has become:
     #
-    #     child[0] not in [all the nodes visited]
+    #     child[1] not in [all the nodes visited]
     #
     # and so on.
     # This sections are, for sake of completeness, indicated
     # by side comments in the code (look for the "here").
     if cutoff < 1:
         return
-    visited = [(source, None)]
+    path = [] # this has been added to separate
+              # visited from path, which caused me trouble
+              # while reasoning (and seems much cleaner)
+    visited = [source]
     # stack = [(v for u,v in gfa_.edges(source))]
-    stack = [((v,k) for u,v,k in selector(source, **args))]
+    stack = [((u,v,k) for u,v,k in selector(source, keys=True))] if keys \
+              else [((u,v) for u,v,k in selector(source, keys=True))]
     while stack:
         children = stack[-1]
         child = next(children, None)
+
         if child is None:
             stack.pop()
             visited.pop()
+
+            # required since at the end and in the
+            # start of the algorithm path will be []
+            if len(path):    # here
+                path.pop()
         elif len(visited) < cutoff:
-            if child[0] == target:    # here
-                yield visited + [(target, None)]
-            elif child[0] not in [v[0] for v in visited]:    # here
-                visited.append(child)
+            if child[1] == target:    # here
+                yield path + [child]
+            elif child[1] not in visited:    # here
+                visited.append(child[1])
+                path.append(child)
                 # stack.append((v for u,v in gfa_.edges(child)))
-                stack.append(((v, k) for u,v,k in selector(child, **args)))
+                add_to_stack = ((u, v, k) for u,v,k in selector(child[1], keys=True)) if keys \
+                                else ((u, v) for u,v,k in selector(child[1], keys=True))
+                stack.append(add_to_stack)
         else: #len(visited) == cutoff:
-            count = ([child[0]]+list(child[0] for child in children)).count(target)    # here
+            count = ([child[1]]+list(child_[1] for child_ in children)).count(target)    # here
             for i in range(count):
-                yield visited + [(target, None)]
+                yield path + [child]
             stack.pop()
             visited.pop()
+            path.pop()
+            if len(path):    # here
+                path.pop()
---
+++ test/test_dovetails_all_simple_paths.py
@@ -0,0 +1,126 @@
+import sys
+sys.path.insert(0, '../')
+
+import copy
+import unittest
+
+import pygfa
+
+gfa_file = "H\tVN:Z:1.0\n" + "S\t1\t*\tLN:i:6871\tRC:i:2200067\n" \
+  + "S\t10\t*\tLN:i:251\tRC:i:82006\n" + "S\t11\t*\tLN:i:208\tRC:i:39533\n" \
+  + "S\t12\t*\tLN:i:186\tRC:i:34457\n" + "S\t16\t*\tLN:i:157\tRC:i:15334\n" \
+  + "S\t18\t*\tLN:i:145\tRC:i:55632\n" + "S\t19\t*\tLN:i:134\tRC:i:49274\n" \
+  + "S\t2\t*\tLN:i:4589\tRC:i:6428225\n" + "S\t20\t*\tLN:i:134\tRC:i:20521\n" \
+  + "S\t21\t*\tLN:i:133\tRC:i:28174\n" + "S\t22\t*\tLN:i:132\tRC:i:17846\n" \
+  + "S\t23\t*\tLN:i:132\tRC:i:24658\n" + "S\t24\t*\tLN:i:107\tRC:i:22256\n" \
+  + "S\t3\t*\tLN:i:2044\tRC:i:2727166\n" + "S\t4\t*\tLN:i:1744\tRC:i:1729157\n" \
+  + "S\t5\t*\tLN:i:1378\tRC:i:1071246\n" + "S\t6\t*\tLN:i:1356\tRC:i:422793\n" \
+  + "S\t7\t*\tLN:i:920\tRC:i:630822\n" + "S\t8\t*\tLN:i:876\tRC:i:794734\n" \
+  + "S\t9\t*\tLN:i:255\tRC:i:40589\n" + "S\t25\t*\tLN:i:1000\n" \
+  + "S\t26\t*\tLN:i:1000\n" + "S\t27\t*\tLN:i:1000\n" \
+  + "S\t28\t*\tLN:i:1000\n" + "L\t25\t+\t26\t+\t10M\n" \
+  + "L\t26\t+\t27\t+\t7M\n" + "L\t27\t+\t28\t+\t10M\n" \
+  + "L\t28\t+\t25\t+\t5M\n" + "S\t13\t*\n" \
+  + "S\t41\t*\n" + "C\t1\t+\t5\t+\t12\t120M\tID:Z:1_to_5\n" \
+  + "P\t15\t41+,13+\t120M\n" + "L\t11\t+\t13\t+\t120M\tID:Z:11_to_13\n" \
+  + "L\t1\t+\t2\t+\t10M\n" + "L\t1\t-\t19\t-\t10M\n" \
+  + "L\t10\t+\t3\t-\t10M\n" + "L\t10\t-\t4\t+\t10M\n" \
+  + "L\t11\t-\t6\t-\t10M\n" + "L\t11\t+\t9\t-\t10M\n" \
+  + "L\t12\t+\t9\t+\t10M\n" + "L\t12\t-\t18\t+\t10M\n" \
+  + "L\t16\t+\t20\t+\t10M\n" + "L\t16\t-\t22\t-\t10M\n" \
+  + "L\t18\t+\t19\t+\t10M\n" + "L\t18\t-\t23\t+\t10M\n" \
+  + "L\t2\t+\t5\t+\t10M\n" + "L\t2\t+\t5\t-\t10M\n" \
+  + "L\t2\t-\t8\t+\t10M\n" + "L\t20\t+\t21\t+\t10M\n" \
+  + "L\t21\t+\t23\t-\t10M\n" + "L\t22\t-\t6\t-\t10M\n" \
+  + "L\t24\t+\t7\t+\t10M\n" + "L\t24\t-\t7\t+\t10M\n" \
+  + "L\t3\t+\t4\t-\t10M\n" + "L\t3\t-\t6\t+\t10M\n" \
+  + "L\t3\t-\t8\t-\t10M\n" + "L\t4\t-\t7\t-\t10M\n"
+
+#
+#
+#                         [8_8_....     ...        ...          ...           ..8_8]
+#            [5         /                                        [13]               \         [10_10]         [24
+#    26       5 \      /                                             \               \       /       \        /24
+#   /  \      5  [2_2_2]                          [12] --- [9] ------ [11]            [3_3_3]        /       / 24
+#  25  27     5 /      \                         /                        \          /       \[4_4_4] --- [7]  24
+#   \  /      5]        [1_1_1] --- [19] --- [18]                          [6_6_6_6]                         \ 24
+#    28                                          \[23]-[21]-[20]-[16]-[22]/                                   \24]
+#
+#                 [41_41_41]
+#
+
+class TestLine (unittest.TestCase):
+
+    graph = pygfa.gfa.GFA()
+    graph.from_string(gfa_file)
+
+    def test_dovetails_all_simple_paths(self):
+        self.assertTrue([("18", "12"),
+                        ("12", "9"),
+                        ("9", "11"),
+                        ("11", "6")]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=True)))
+
+        self.assertTrue([("18", "23"),
+                        ("23", "21"),
+                        ("21", "20"),
+                        ("20", "16"),
+                        ("16", "22"),
+                        ("22", "6")]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=True)))
+
+        self.assertTrue([("18", "19"),
+                        ("19", "1"),
+                        ("1", "2"),
+                        ("2", "8"),
+                        ("8", "3"),
+                        ("3", "6")]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=True)))
+        self.assertTrue(len(list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=True))) == 3)
+        self.assertTrue(["18", "12", "9", "11", "6"]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=False)))
+
+        self.assertTrue(["18", "23", "21", "20", "16", "22", "6"]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=False)))
+
+        self.assertTrue(["18", "19", "1", "2", "8", "3", "6"]
+                        in list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=False)))
+        self.assertTrue(len(list(pygfa.dovetails_all_simple_paths(\
+                                                    self.graph,
+                                                    "18",
+                                                    "6",
+                                                    edges=True))) == 3)
+
+
+        
+
+if  __name__ == '__main__':
+    unittest.main()
