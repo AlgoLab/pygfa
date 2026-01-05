221363f418a09f4db54fc724818b3e3e1cfa00c2
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-01-05 07:31:58 +0100

    feat: add script to convert BGFA format to BGFA format

---
+++ doc/source/to_bgfa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# AI! write a program that reads a GFA file, whose file name is given as a
+# command line option, and outputs a BGFA file, whose file name is given as a
+# command line option, using the write_bgfa method
