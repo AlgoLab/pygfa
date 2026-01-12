17a6250121430d9908b761e1daac8e75c229cf80
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-01-12 16:52:32 +0100

    feat: add GFA prettification tool

---
+++ tools/prettify_gfa.py
@@ -0,0 +1,3 @@
+# AI! write a python program that receives a command line option that is the
+# filename of a gfa file, reads the file using the from_gfa method and pretty
+# prints the resulting graph, using the pprint method
