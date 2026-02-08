ecedc8392a1af6237ece2fb3ff4ac42d5e42328c
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-02-08 09:58:59 +0100

    remove gfa 2

--- pygfa/graph_element/parser/__init__.py
+++ pygfa/graph_element/parser/__init__.py
@@ -5,6 +5,12 @@ from pygfa.graph_element.parser.link import Link
 from pygfa.graph_element.parser.path import Path
 from pygfa.graph_element.parser.segment import SegmentV1
 
+# Remove removed modules imports
+# from pygfa.graph_element.parser.fragment import Fragment  # GFA2 - REMOVED
+# from pygfa.graph_element.parser.edge import Edge  # GFA2 - REMOVED
+# from pygfa.graph_element.parser.gap import Gap  # GFA2 - REMOVED
+# from pygfa.graph_element.parser.group import Group  # GFA2 - REMOVED
+
 __all__ = [
     "containment",
     "header",
