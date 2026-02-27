2fea14512f91c43de3dfff1f3f30b518ad1a8441
Gianluca Della Vedova <gianluca@dellavedova.org>
2026-02-27 21:55:56 +0100

    show-full-encodings options

--- pygfa/encoding/__init__.py
+++ pygfa/encoding/__init__.py
@@ -174,3 +174,98 @@ from pygfa.encoding.ppm_coding import (
     compress_string_ppm as compress_string_ppm,
     compress_string_list_ppm as compress_string_list_ppm,
 )
+
+COMPRESSION_OPTIONS: dict[str, list[str]] = {
+    "segment_names": [
+        "segment_names_header",
+        "segment_names_payload_lengths",
+        "segment_names_payload_names",
+    ],
+    "segments": [
+        "segments_header",
+        "segments_payload_lengths",
+        "segments_payload_strings",
+    ],
+    "links": [
+        "links_header",
+        "links_payload_from",
+        "links_payload_to",
+        "links_payload_cigar_lengths",
+        "links_payload_cigar",
+    ],
+    "paths": [
+        "paths_header",
+        "paths_payload_names",
+        "paths_payload_segment_lengths",
+        "paths_payload_path_ids",
+        "paths_payload_cigar_lengths",
+        "paths_payload_cigar",
+    ],
+    "walks": [
+        "walks_header",
+        "walks_payload_sample_ids",
+        "walks_payload_hep_indices",
+        "walks_payload_sequence_ids",
+        "walks_payload_start",
+        "walks_payload_end",
+        "walks_payload_walks",
+    ],
+}
+
+INTEGER_ENCODING_NAMES: list[str] = [e.name.lower() for e in IntegerEncoding]
+STRING_ENCODING_NAMES: list[str] = [e.name.lower() for e in StringEncoding]
+
+INTEGER_ENCODINGS: dict[str, str] = {
+    "none": "compress_integer_list_none",
+    "varint": "compress_integer_list_varint",
+    "fixed16": "compress_integer_list_fixed",
+    "fixed32": "compress_integer_list_fixed",
+    "fixed64": "compress_integer_list_fixed",
+    "delta": "compress_integer_list_delta",
+    "gamma": "compress_integer_list_elias_gamma",
+    "omega": "compress_integer_list_elias_omega",
+    "golomb": "compress_integer_list_golomb",
+    "rice": "compress_integer_list_rice",
+    "streamvbyte": "compress_integer_list_streamvbyte",
+    "vbyte": "compress_integer_list_vbyte",
+    "pfor_delta": "compress_integer_list_pfor_delta",
+    "simple8b": "compress_integer_list_simple8b",
+    "group_varint": "compress_integer_list_group_varint",
+    "bit_packing": "compress_integer_list_bitpacking",
+    "fibonacci": "compress_integer_list_fibonacci",
+    "exp_golomb": "compress_integer_list_exp_golomb",
+    "byte_packed": "compress_integer_list_byte_packed",
+    "masked_vbyte": "compress_integer_list_masked_vbyte",
+    "": "compress_integer_list_none",
+}
+
+STRING_ENCODINGS: dict[str, str] = {
+    "none": "compress_string_none",
+    "zstd": "compress_string_zstd",
+    "zstd_dict": "compress_string_zstd_dict",
+    "gzip": "compress_string_gzip",
+    "lzma": "compress_string_lzma",
+    "lz4": "compress_string_lz4",
+    "brotli": "compress_string_brotli",
+    "huffman": "compress_string_list_huffman",
+    "frontcoding": "compress_string_list_frontcoding",
+    "delta": "compress_string_list_delta",
+    "dictionary": "compress_string_list_dictionary",
+    "rle": "compress_string_rle",
+    "cigar": "compress_string_cigar",
+    "2bit": "compress_string_2bit_dna",
+    "arithmetic": "compress_string_arithmetic",
+    "bwt_huffman": "compress_string_bwt_huffman",
+    "ppm": "compress_string_ppm",
+    "": "compress_string_none",
+}
+
+
+def show_full_encodings() -> dict[str, list[str]]:
+    """Return a flat dict mapping each compression option to its valid encoding methods."""
+    all_methods = sorted(INTEGER_ENCODING_NAMES + STRING_ENCODING_NAMES)
+    result: dict[str, list[str]] = {}
+    for options in COMPRESSION_OPTIONS.values():
+        for opt in options:
+            result[opt] = all_methods
+    return result
